14f4291d81f4225ccf588fe7e8b667a1e84ed459
AmelMhdi <amel.m20@gmail.com>
2025-08-21 16:24:58 +0200

    update: added new routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './auth/AuthContext';
 
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import Leaderboard from './pages/Leaderboard';
 import SingleTake from './pages/SingleTake';
-import { AuthProvider } from './auth/AuthContext';
+import DebateRoom from './pages/DebateRoom';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
@@ -17,6 +18,8 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/leaderboard" element={<Leaderboard />} />
+            <Route path="/take/:id" element={<SingleTake />} />
+            <Route path="/debate/:id" element={<DebateRoom />} />
             <Route path="/take/:id" element={<SingleTake />} />
           </Routes>
         </Layout>
